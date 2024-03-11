caf018b00a94db93c3dcfccc108963e7edb0a57f
unknown <Elqoptan337@gmail.com>
2024-03-11 16:33:09 +0200

    this is my js task

---
+++ Task.js
@@ -0,0 +1,27 @@
+var question = prompt("do you fly")
+if (question == "yes")
+{
+    var question2 = prompt("Are you wild")
+    if (question2 == "yes")
+    alert("Eagle")
+    if (question2 == "no")
+    alert("Parrot")
+}
+if (question == "no")
+{
+    var question3 = prompt("Do you live under the sea?")
+    if (question3 == "yes")
+    var question4 = prompt("Are you Wild?")
+    if (question4 == "yes")
+    alert("shark")
+    if (question4 == "no")
+    alert("dolphon")
+    ///////////////
+    if (question == "no")
+    var question5 = prompt("are you wild?")
+    if (question5 == "yes")
+    alert("lion")
+    if (question5 == "no")
+    alert("cat")
+
+}
